a8806f93f0b148e7e1f181b177e7293122e67821
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-21 08:23:40 -0300

    [feat] import header and theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import * as React from "react"
 import { ChakraProvider } from "@chakra-ui/react"
-
+import theme from '../styles/theme'
+import Header from "../components/Header"
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme} >
+      <Header/>
       <Component {...pageProps} />
     </ChakraProvider>
   )
