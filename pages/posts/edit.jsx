0ff5e242d3baf453e37e7e05cda4fe409406a24e
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-24 11:45:33 -0300

    improve edit page

--- pages/posts/edit.jsx
+++ pages/posts/edit.jsx
@@ -16,48 +16,105 @@ import {
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useState, useRef } from "react";
+
 const EditPost = () => {
-    const { handleSubmit, control, errors } = useForm();
-    const toast = useToast();
-    const router = useRouter();
-    const id  = router.query;
+  const { handleSubmit, control, errors } = useForm();
+  const toast = useToast();
+  const router = useRouter();
+  const { id } = router.query;
 
-    const post = { title:"Titulo do Post", content: "Conteudo do post" }
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = useRef();
 
-    const onSubmit = (data) => {
-        // Aqui você faria a chamada à API para atualizar a postagem.
-        // Por enquanto, apenas exibimos um toast de sucesso.
-        toast({
-            title:"Postagem atualizada.",
-            description:`A postagem "${data.title}" foi atualizada com sucesso`,
-            status:"sucess",
-            duration:3000,
-            isClosable:true,
-        });
-    };
+  // Substitua isto pela sua chamada à API para obter os dados da postagem.
+  const post = { title: "Título do post", content: "Conteúdo do post" };
 
-    return (
-        <Box>
-            <Box p={5}>
-                <form onSubmit={handleSubmit(onSubmit)} >
-                    <FormControl id="title" isRequired >
-                        <FormLabel>Título</FormLabel>
-                        <Controller 
-                            name="title"
-                            control={control}
-                            defaultValue={post.title}
-                            rules={{required:"O titulo é obrigatório"}}
-                            render={({ field }) => <Input {...field} />}
-                        />
-                        {/*errors.content && <span>{errors.content.message}</span>*/}
-                    </FormControl>
-                    <Button type='submit' colorScheme="blue" mt={4}>
-                        Atualizar
-                    </Button>
-                </form>
-            </Box>
-        </Box>
-    )
-}
+  const onSubmit = (data) => {
+    // Aqui você faria a chamada à API para atualizar a postagem.
+    // Por enquanto, apenas exibimos um toast de sucesso.
+    toast({
+      title: "Postagem atualizada.",
+      description: `A postagem "${data.title}" foi atualizada com sucesso.`,
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
+
+  const onDelete = () => {
+    // Aqui você faria a chamada à API para excluir a postagem.
+    // Por enquanto, apenas exibimos um toast de sucesso e redirecionamos o usuário para a página inicial.
+    toast({
+      title: "Postagem excluída.",
+      description: `A postagem "${post.title}" foi excluída com sucesso.`,
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+    router.push("/");
+  };
+
+  return (
+    <Box>
+      <Box p={5}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl id="title" isRequired>
+            <FormLabel>Título</FormLabel>
+            <Controller
+              name="title"
+              control={control}
+              defaultValue={post.title}
+              rules={{ required: "O título é obrigatório." }}
+              render={({ field }) => <Input {...field} />}
+            />
+            {/* errors.title && <span>{errors.title.message}</span>*/}
+          </FormControl>
+          <FormControl id="content" isRequired mt={4}>
+            <FormLabel>Conteúdo</FormLabel>
+            <Controller
+              name="content"
+              control={control}
+              defaultValue={post.content}
+              rules={{ required: "O conteúdo é obrigatório." }}
+              render={({ field }) => <Textarea {...field} />}
+            />
+            {/* errors.content && <span>{errors.content.message}</span>*/}
+          </FormControl>
+          <Button type="submit" colorScheme="blue" mt={4}>
+            Atualizar
+          </Button>
+          <Button colorScheme="red" ml={4} onClick={() => setIsOpen(true)}>
+            Excluir
+          </Button>
+        </form>
+        <AlertDialog
+          isOpen={isOpen}
+          leastDestructiveRef={cancelRef}
+          onClose={onClose}
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                Excluir Postagem
+              </AlertDialogHeader>
+              <AlertDialogBody>
+                Tem certeza que deseja excluir a postagem "{post.title}"? Esta ação não pode ser desfeita.
+              </AlertDialogBody>
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  Cancelar
+                </Button>
+                <Button colorScheme="red" onClick={onDelete} ml={3}>
+                  Excluir
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </Box>
+    </Box>
+  );
+};
 
 export default EditPost;
