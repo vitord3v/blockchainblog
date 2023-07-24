08585835f00277cebe519ee3ebab3061a2ac46f1
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-24 11:44:14 -0300

    [feat] ajust css styles

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const posts = [
     id: 1,
     title: "Introdução à Web3",
     excerpt: "Este artigo apresenta a Web3...",
-  },
+  }, 
   {
     id: 2,
     title: "Por que Blockchain é revolucionário",
@@ -16,15 +16,15 @@ const posts = [
 const Home = () => {
   return (
     <Box>
-      <Box p={5}>
-        <Heading as="h1" mb={5}>Postagens Recentes</Heading>
-        {posts.map((post) => (
-          <Box key={post.id} p={5} shadow="md" borderWidth="1px" rounded="md">
-            <Heading as="h2">{post.title}</Heading>
-            <Text mt={4}>{post.excerpt}</Text>
-          </Box>
-        ))}
-      </Box>
+      <Heading as="h1" mb={'2rem'} mt={'2rem'}>Postagens Recentes</Heading>
+        <Box p={5} display='flex' gap='2rem' flexDir='column'>
+          {posts.map((post) => (
+            <Box key={post.id} p={5} shadow="md" borderWidth="1px" rounded="md" display='flex' alignItems='center' justifyContent='center' flexDir='column'>
+              <Heading as="h2">{post.title}</Heading>
+              <Text mt={4}>{post.excerpt}</Text>
+            </Box>
+          ))}
+        </Box>
     </Box>
   );
 };
