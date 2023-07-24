a8001d2578abdb13017970cb0de5e15b2a339e1c
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-24 10:49:14 -0300

    Refactor and feat of nav search bar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,44 @@
-import { Box, Link } from "@chakra-ui/react";
+import { useState } from "react";
+import { Box, Link, Input, Button, FormControl,Flex } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
-const Header = () => (
-    <Box bg="blue.800" w="100%" p={4} color="white">
-        <Link href="/" mr={5}>Home</Link>
-        <Link href="/about" mr={5}>Sobre</Link>
-        <Link href="/contact" mr={5}>Contato</Link>
-    </Box>
-);
+const Header = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const router = useRouter();
+
+  const onSearchSubmit = (e) => {
+    e.preventDefault();
+
+    // Redirecionar o usuário para a rota /search com o termo de pesquisa como parâmetro de query.
+    router.push(`/search?term=${encodeURIComponent(searchTerm)}`);
+  };
+
+  return (
+    <Flex bg="purple.800" w="100%" alignItems='center'  p={4} color="white"  fontWeight="bold">
+      <Link href="/" mr={5}>
+        Inicio
+      </Link>
+      <Link href="/about" mr={5}>
+        Bombando
+      </Link>
+      <Link href="/about" mr={5}>
+        Todos
+      </Link>
+      <FormControl display="flex" ml={5} onSubmit={onSearchSubmit}>
+          <Input
+            placeholder="Nome do artigo"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            mr={2}
+            display="inline-block"
+            width="200px"
+          />
+          <Button type="submit" display="inline-block">
+            Pesquisar
+          </Button>
+      </FormControl>
+    </Flex>
+  );
+};
 
 export default Header;
