5abcfb82024025f91e9648d22be05e8483bd27e2
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-25 07:50:45 -0300

    [feat] add comment post

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,41 +1,51 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
-import { useRouter } from "next/router";
-import Header from "../../components/Header";
-
-// Este é um exemplo de dados para as postagens do blog. Substitua isto pela sua chamada à API.
-const posts = [
-  {
-    id: 1,
-    title: "Introdução à Web3",
-    content: "Este artigo apresenta a Web3...",
-  },
-  {
-    id: 2,
-    title: "Por que Blockchain é revolucionário",
-    content: "Neste post, vamos explorar por que o blockchain...",
-  },
-  // Adicione mais postagens conforme necessário
-];
+import { Box, Input, Button, FormControl, FormLabel, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+import Comment from "../../components/Comment";
 
 const Post = () => {
-  const router = useRouter();
-  const { id } = router.query;
+  const [comments, setComments] = useState([]); // No início, não temos nenhum comentário
+  const [newComment, setNewComment] = useState("");
 
-  // Encontra a postagem correspondente ao id da rota.
-  const post = posts.find((post) => post.id === Number(id));
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  // Se a postagem não foi encontrada, retorna null.
-  if (!post) {
-    return null;
-  }
+    // Aqui você pode fazer a chamada à API para adicionar o novo comentário
+    // E depois atualizar a lista de comentários local
+    // Por simplicidade, estamos apenas adicionando o novo comentário localmente
+    setComments([
+      ...comments,
+      {
+        author: "Usuário",
+        text: newComment,
+      },
+    ]);
+    setNewComment("");
+  };
 
   return (
-    <Box>
-      <Box p={5}>
-        <Heading as="h1" mb={5}>{post.title}</Heading>
-        <Text mt={4}>{post.content}</Text>
+    <VStack align="stretch" maxW="md" mx="auto" mt={10} spacing={6}>
+      <Box borderWidth="1px" borderRadius="lg" p={4}>
+        Conteúdo da postagem...
       </Box>
-    </Box>
+
+      <form onSubmit={handleSubmit}>
+        <FormControl id="comment" isRequired>
+          <FormLabel>Deixe um comentário</FormLabel>
+          <Input
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            placeholder="Digite seu comentário aqui"
+          />
+        </FormControl>
+        <Button mt={4} colorScheme="teal" type="submit">
+          Postar
+        </Button>
+      </form>
+
+      {comments.map((comment, index) => (
+        <Comment key={index} comment={comment} />
+      ))}
+    </VStack>
   );
 };
 
